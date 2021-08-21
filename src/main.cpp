ee4d371136f6ca823787c37187d1f0c449a468e7
34-anish <luckyanish47@gmail.com>
2021-08-21 22:02:05 +0545

    Exception handled

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+#include "PrefixToPostfix.h"
+using namespace std;
+
+int main()
+
+{ //Displays all the process of conversion of prefix to postfix expression
+
+    try
+    {
+        std::cout << std::endl;
+        string pre_exp = "+ A * B C D ";
+        std::cout << "PREFIX OF :   " << pre_exp << endl
+                  << std::endl;
+        std::cout << "Reading the string from right to left order...." << std::endl;
+        std::cout << PrefixToPostfix(pre_exp) << std::endl;
+        std::cout << std::endl;
+    }
+
+    catch (const char *error)
+    {
+        std::cerr << error << std::endl;
+    }
+}
